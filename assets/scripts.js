8a3eb80e749961d2ab558f31935a735d4c1b4383
Chris Elliott <acelliott1218@gmail.com>
2024-05-09 17:00:47 +0000

    add dialogue and image functionality to game, character stat integration coming next

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,9 @@
 const textElement = document.getElementById("text");
+const dialogueElement = document.getElementById("dialogue")
+const imageElement = document.getElementById("image")
 const optionButtonsElement = document.getElementById("option-buttons")
 
-
 let state = {}
-
 
 //**starts the game when called */ 
 function startGame(){
@@ -14,7 +14,9 @@ function startGame(){
 //**shows a particular text node in relation to user input, while hiding others */
 function showTextNode(textNodeIndex) {
     const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
-    textElement.innerText = textNode.text;
+    textElement.innerHTML = textNode.text;
+    dialogueElement.innerHTML = textNode.dialogue
+    imageElement.innerHTML = textNode.image;
     while (optionButtonsElement.firstChild){
         optionButtonsElement.removeChild(optionButtonsElement.firstChild);
     }
@@ -29,6 +31,7 @@ function showTextNode(textNodeIndex) {
     })
 }
 
+
 //**reveals options on click if the correct conditions are met */
 function showOption(option){
     return option.requiredState == null || option.requiredState(state);
@@ -41,21 +44,25 @@ function selectOption(option){
     showTextNode(nextTextNodeId);
 }
 
+
 // this variable acts sort of like a function, but is required to circumvent excessive "if-else" statements. 
 // It's on this line to help the flow/readability of the code
 const textNodes = [
     {
         id: 1,
-        text: 'Whoa, blue goo!',
+        text: 'You\'ve arrived as a stranger in a strange land. On the floor in front of you, however, there\'s a golden coin. An old man points it out to you, saying...',
+        image: '<img src="assets/images/new years.jpg" alt="An old man">',
+        dialogue: `<h2 class="title">Hello there!</h2> <br> <p class="title">You're not from around here, so I'll warn you; not picking up a coin's bad luck.</p>`,
         options: [
             // these are the first stage of the game
             {
-                text: 'Take the goo',
-                setState:{blueGoo: true},
+                text: 'Pick up the coin',
+                setState:{coin: true},
                 nextText: 2,
             },
             {
-                text: 'Leave the goo',
+                text: 'Ignore the coin',
+                setState:{badLuck: true},
                 nextText: 2,
             }
         ]
@@ -63,25 +70,27 @@ const textNodes = [
     {
         id: 2,
         text:'You venture forth in search of answers to where you are when you come across a merchant',
+        image: '<img src="assets/images/new years.jpg" alt="An old man">',
+        dialogue: `<h2 class="title">Hello there!</h2> <br> <p class="title">You're not from around here, so I'll warn you; not picking up a coin's bad luck.</p>`,
         options: [
             {
                 text: 'Trade the goo for a sword',
                 // requires the user to have taken the blue goo
-                requiredState: (currentState) => currentState.blueGoo,
-                setState: {blueGoo: false, sword: true},
+                requiredState: (currentState) => currentState.coin,
+                setState: {coin: false, sword: true},
                 nextText: 3
             },
             {
                 text: 'Trade the goo for a shield',
                 // requires the user to have taken the blue goo
-                requiredState: (currentState) => currentState.blueGoo,
-                setState:{blueGoo: false, shield: true},
+                requiredState: (currentState) => currentState.coin,
+                setState:{coin: false, shield: true},
                 nextText: 3
             },
             {
                 // this is the option if the user has no goo to trade
                 text: 'Ignore the merchant',
-                setState:{blueGoo: false, sword: true},
+                setState:{coin: false, badLuck:true, sword: false},
                 nextText: 3,
             }
         ]
@@ -89,6 +98,8 @@ const textNodes = [
     {
         id: 3,
         text: 'After leaving you find a castle',
+        image: '<img src="assets/images/new years.jpg" alt="An old man">',
+        dialogue: `<h2 class="title">Hello there!</h2> <br> <p class="title">You're not from around here, so I'll warn you; not picking up a coin's bad luck.</p>`,
         options: [
             {
                 text: 'explore',
@@ -107,6 +118,8 @@ const textNodes = [
     {
         id: 4,
         text: 'You fall asleep and die',
+        image: '<img src="assets/images/new years.jpg" alt="An old man">',
+        dialogue: `<h2 class="title">Hello there!</h2> <br> <p class="title">You're not from around here, so I'll warn you; not picking up a coin's bad luck.</p>`,
         options: [
             {
                 text: 'Retry?',
